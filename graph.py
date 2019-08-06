3274ad77d5e5b377b12e9be1bb4b2e9b634c692f
petermaj <peperowing@gmail.com>
2019-08-06 14:47:09 +0200

    I created some button without function and one canvas.

--- graph.py
+++ graph.py
@@ -1,10 +1,11 @@
-from tkinter import *
-class Window(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.master=master
-    
+from tkinter import *    
 
-root=Tk()        
-app=Window(root)
-root.mainloop()
+window=Tk()  
+window.title("Workout Datas")      
+button_speed=Button(window, text="Show speed datas") #, command=show_speed)
+button_heartrate=Button(window, text="Show heartrate datas") #, command=show_heartrate)
+button_speed.grid(row=0, column=0)
+button_heartrate.grid(row=1, column=0)
+canvas=Canvas(window, width=360, height=360, bg='white')
+canvas.grid(row=0, rowspan=4, column=1)
+window.mainloop()

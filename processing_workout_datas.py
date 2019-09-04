011de86ae018ceb1aa475641396604f709985634
petermaj <peperowing@gmail.com>
2019-09-04 14:41:02 +0200

    In 12th line I use the function what return a worksheet names and I use the names array in 20th line.

--- processing_workout_datas.py
+++ processing_workout_datas.py
@@ -9,6 +9,7 @@ class workoutData:
         
 def processing_workout_datas():
     workbook=getWorkoutData.openWorkbook("workoutdata/Move_2019_07_29_17_38_24_Cycling.xlsx")
+    worksheetnames=getWorkoutData.getworksheetnames(workbook)
     row=getWorkoutData.getDatas(workbook,2)
     avgspeed=row[8]
     avgheartrate=row[5]
@@ -16,7 +17,7 @@ def processing_workout_datas():
     data=workoutData(row[53], row[54], row[55])
     datas.append(data)
     index=3
-    while(index!=getWorkoutData.getRowNumber(workbook, "29 Jul 2019 17_38_24")):
+    while(index!=getWorkoutData.getRowNumber(workbook, worksheetnames[0])):
         row=getWorkoutData.getDatas(workbook,index)
         if(row[53]!=''):
             data=workoutData(row[53], row[54], row[55])    
